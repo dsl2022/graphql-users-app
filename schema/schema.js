c2f86663df1010c8f71da26ffc7a1b1f27c1010e
tmm <blocklocmedia@gmail.com>
2019-09-08 23:42:04 -0400

    created schema folder and schema.js

---
+++ schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const userType = new GraphQLObjectType({
+	name: 'User',
+	fields: {
+		id: { type: GraphQLString },
+		firstName: { type: GraphQLString },
+		age: { type: GraphQLInt }
+	}
+});
